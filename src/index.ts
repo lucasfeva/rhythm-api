544469ecd25cfa7fa10b8e3f350a60b907a2d4a0
Lucasfeva <luscas.png@gmail.com>
2026-02-25 13:45:44 -0300

    chore: add fastify swagger generation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import Fastify from "fastify";
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
 } from "fastify-type-provider-zod";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
 import z from "zod";
 
 const app = Fastify({
@@ -12,6 +15,27 @@ const app = Fastify({
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
+
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Rhythm API",
+      description: "API for Rhythm app",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        description: "Local server",
+        url: "http://localhost:3333",
+      },
+    ],
+  },
+  transform: jsonSchemaTransform,
+});
+
+await app.register(fastifySwaggerUI, {
+  routePrefix: "/docs",
+});
 
 app.withTypeProvider<ZodTypeProvider>().route({
   method: "GET",
