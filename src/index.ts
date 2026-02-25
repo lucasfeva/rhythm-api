2a3347577b007b8fd91c2bc9b683e4e98f330921
Lucasfeva <luscas.png@gmail.com>
2026-02-25 13:30:16 -0300

    feat: add fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello, world!");
+import Fastify from "fastify";
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async function handler(request, reply) {
+  return { hello: "world" };
+});
+
+try {
+  await fastify.listen({ port: Number(process.env.PORT) || 3333 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
